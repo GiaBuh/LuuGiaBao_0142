11c54701a03293f27dad9ac85fd09313816a8b1e
giabao <baoluu674.com>
2026-03-24 14:03:20 +0700

    LAB_03_EX02

---
+++ Lab_03/api.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify
+from cipher.rsa import RSACipher
+
+app = Flask(__name__)
+
+rsa_cipher = RSACipher()
+
+@app.route('/api/rsa/generate', methods=['GET'])
+def generate_keys():
+    rsa_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/rsa/encrypt', methods=['POST'])
+def encrypt():
+    data = request.get_json()
+    message = data['message']
+    key = data['key_type']
+    private_key, public_key = rsa_cipher.load_keys()
+    if key == 'public':
+        key = public_key
+    elif key == 'private':
+        key = private_key
+    else:
+        return jsonify({'error': 'Invalid key'})
+    encrypted_message = rsa_cipher.encrypt(message, key)
+    encrypted_hex = encrypted_message.hex()
+    return jsonify({'encrypted_message': encrypted_hex})
+
+@app.route('/api/rsa/decrypt', methods=['POST'])
+def decrypt():
+    data = request.get_json()
+    ciphertext = data['ciphertext']
+    key = data['key_type']
+    private_key, public_key = rsa_cipher.load_keys()
+    if key == 'public':
+        key = public_key
+    elif key == 'private':
+        key = private_key
+    else:
+        return jsonify({'error': 'Invalid key'})
+    ciphertext = bytes.fromhex(ciphertext)
+    decrypted_message = rsa_cipher.decrypt(ciphertext, key)
+    return jsonify({'decrypted_message': decrypted_message})
+
+@app.route('/api/rsa/sign', methods=['POST'])
+def sign():
+    data = request.get_json()
+    message = data['message']
+    private_key, public_key = rsa_cipher.load_keys()
+    signature = rsa_cipher.sign(message, private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
+
+@app.route('/api/rsa/verify', methods=['POST'])
+def verify():
+    data = request.get_json()
+    message = data['message']
+    signature = data['signature']
+    private_key, public_key = rsa_cipher.load_keys()
+    signature = bytes.fromhex(signature)
+    is_verified = rsa_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verified': is_verified})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
